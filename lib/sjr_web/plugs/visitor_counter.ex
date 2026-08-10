51da6b66b133cbba0460c878084aaaf054f05a7a
Simão Júnior <simao.msjr@gmail.com>
2026-08-10 19:41:08 -0300

    fix: count visitor once per session instead of per page view

--- lib/sjr_web/plugs/visitor_counter.ex
+++ lib/sjr_web/plugs/visitor_counter.ex
@@ -1,13 +1,19 @@
 defmodule SjrWeb.Plugs.VisitorCounter do
-  @moduledoc "Increments the visitor counter once per page view."
+  @moduledoc "Increments the visitor counter once per browser session."
   @behaviour Plug
+
+  import Plug.Conn
 
   @impl Plug
   def init(opts), do: opts
 
   @impl Plug
   def call(conn, _opts) do
-    Sjr.VisitorCounter.increment()
-    conn
+    if get_session(conn, :counted) do
+      conn
+    else
+      Sjr.VisitorCounter.increment()
+      put_session(conn, :counted, true)
+    end
   end
 end
