ea3fe79fd5c82c1814a46558360a986391982461
Simão Júnior <simao.msjr@gmail.com>
2026-08-10 01:35:29 -0300

    test: update homepage assertion for the new content

--- test/sjr_web/controllers/page_controller_test.exs
+++ test/sjr_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SjrWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Mission log"
   end
 end
